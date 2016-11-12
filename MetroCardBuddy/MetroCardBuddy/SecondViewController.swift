e97e67d85890b86bb12fd60112413b9ab1201014
Aditya <aditya.s.bhat@gmail.com>
2016-11-12 14:17:50 -0500

    initial tabbed application template commit

---
+++ MetroCardBuddy/MetroCardBuddy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MetroCardBuddy
+//
+//  Created by Aditya Bhat on 11/12/16.
+//  Copyright © 2016 Aditya Bhat. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
